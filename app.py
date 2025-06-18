ad10e827ed9445732f31efa956ebf6f4ff7f0f07
Abhilash <abhilashyellenki@gmail.com>
2025-06-18 12:42:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -67,5 +67,7 @@ def admin():
 def download():
     return send_file("responses.csv", as_attachment=True)
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
